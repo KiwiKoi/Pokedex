5fa7b7b13233694216fc84a951d3b7c50f2581a1
Daniel <danielo.visage@gmail.com>
2020-06-30 10:38:07 +0200

    Updates

---
+++ precache-manifest.9083502c6b606cebb51b3d1789196d80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98c90676501c9f7b7c3b4ec53f2b2b76",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "6dbd93ee43a18cd4160b",
+    "url": "/pokedex/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3680afa8bf59f44a5cb8",
+    "url": "/pokedex/static/css/main.f28e1161.chunk.css"
+  },
+  {
+    "revision": "6dbd93ee43a18cd4160b",
+    "url": "/pokedex/static/js/2.f48f7f0b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokedex/static/js/2.f48f7f0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3680afa8bf59f44a5cb8",
+    "url": "/pokedex/static/js/main.c0394491.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  }
+]);
